9620ac361e5fb066911e36985c96609950983a68
kingbird <4739345@qq.com>
2020-12-28 11:30:04 +0800

    add vue cli

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+    <div id="app">
+        <div class="sidebar">
+            <logo s3link="https://s3dir.com/" s3name="s3dir" />
+        </div>
+    </div>
+</template>
+
+<script>
+import logo from './components/Logo.vue';
+export default {
+    name: 'App',
+    components: {
+        logo,
+    },
+};
+</script>
+
+<style lang="less">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Logo.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="logo">
+        <a :href="s3link" :title="s3name">Logo</a>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Logo',
+    props: {
+        s3link: {
+            type: String,
+            default: ''
+        },
+        s3name: {
+            type: String,
+            default: ''
+        }
+    }
+};
+</script>
