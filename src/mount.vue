1658df258dae3f3fca2ad00053b780cb1bb4f269
kingbird <4739345@qq.com>
2021-01-03 12:52:48 +0800

    use vue

--- src/mount.vue
+++ src/mount.vue
@@ -33,8 +33,8 @@ export default {
     methods: {
         getService() {
             let cos = new COS({
-                SecretId: 'AKIDPnwpTt7zfLnzwdHKK5qOhlaBXeXSlalg',
-                SecretKey: 'rTpvxmMZZMtBDF2ZsvkKeilbCYfnmZ7b'
+                SecretId: localStorage.getItem('SecretId'),
+                SecretKey: localStorage.getItem('SecretKey')
             });
             // let vm = this;
             cos.getService({
